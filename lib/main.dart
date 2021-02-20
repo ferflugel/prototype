951aefbf09b590d692b8e487ad4683ed2afc0ff3
Fernando Flugel Assad <fernandofassad@gmail.com>
2021-02-20 08:50:50 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,93 +6,114 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Hey Guys',
       theme: ThemeData(
         primaryColor: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Emotion Recognition"),
-        ),
-        body: Center( 
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  Widget _buildSuggestions() {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            'testing.png',
+            width: 100,
+            height: 100,
+            fit: BoxFit.contain,
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Image.asset(
-                'testing.png',
-                width: 100,
-                height: 100,
-                fit: BoxFit.contain,
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
+              Row(
                 children: [
-                  Row(
-                    children:[
-                    Text('Language Selection',
-                    style: TextStyle( 
+                  Text(
+                    'Language Selection',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  PopupMenuButton(
+                    // THIS LINE IS STILL BUGGY onSelected: (WhyFarther result) { setState(() { _selection = result; }); },
+                    itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                      const PopupMenuItem(
+                        value: 1,
+                        child: Text('English'),
+                      ),
+                      const PopupMenuItem(
+                        value: 2,
+                        child: Text('French'),
+                      ),
+                      const PopupMenuItem(
+                        value: 3,
+                        child: Text('Italian'),
+                      ),
+                      const PopupMenuItem(
+                        value: 4,
+                        child: Text('Portuguese'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.all(15),
+                padding: EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Transcript',
+                      style: TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    PopupMenuButton(
-                      // THIS LINE IS STILL BUGGY onSelected: (WhyFarther result) { setState(() { _selection = result; }); },
-                      itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-                        const PopupMenuItem(
-                          value: 1,
-                          child: Text('English'),
-                        ),
-                        const PopupMenuItem(
-                          value: 2,
-                          child: Text('French'),
-                        ),
-                        const PopupMenuItem(
-                          value: 3,
-                          child: Text('Italian'),
-                        ),
-                        const PopupMenuItem(
-                          value: 4,
-                          child: Text('Portuguese'),
-                        ),
-                      ],
+                    Container(
+                      margin: EdgeInsets.all(5),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      width: 200,
+                      child: Text(
+                          'Here we will have the transcript test. I am not sure how would we import this text, however.'),
                     ),
-                    ],
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(15),
-                    padding: EdgeInsets.all(10),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10),
-                      ),
-                    child: Column(
-                    children: [
-                      Text('Transcript',
-                        style: TextStyle( 
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(5),
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        width: 200,
-                        child: Text('Here we will have the transcript test. I am not sure how would we import this text, however.'),
-                      ),
-                    ],
-                    ),
-                  ), 
-                ],
-              )
+                  ],
+                ),
+              ),
             ],
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Emotion Recognition'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+}
+// #enddocregion RWS-var
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
